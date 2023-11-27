84d05fabf3025906c9d5515a5fcec0886c7515c9
Gabriel Biler <gabsbiler@gmail.com>
2023-11-27 18:26:59 +0800

    Authentication API Initialized

--- resources/ts/plugins/axios.ts
+++ resources/ts/plugins/axios.ts
@@ -1,10 +1,10 @@
-import axios from 'axios'
 import router from '@/router'
+import axios from 'axios'
 
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  // baseURL: 'https://some-domain.com/api/',
+  baseURL: 'http://127.0.0.1:8000/',
   // timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
 })
