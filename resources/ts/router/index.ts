ca3dfb98ae3cefd1b58f5a46fed9872b1d8791fa
Gabriel Biler <gabsbiler@gmail.com>
2024-02-01 07:01:01 +0800

    update

--- resources/ts/router/index.ts
+++ resources/ts/router/index.ts
@@ -3,8 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import routes from '~pages';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  // history: createWebHistory('/'),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/'),
   // history: createWebHistory('/'),
   routes: [
     ...setupLayouts(routes),
