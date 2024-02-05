f287f030d9aca46547424aed4ef57b1d41e60f9a
Gabriel Biler <gabsbiler@gmail.com>
2024-02-05 18:08:58 +0800

    update

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://ibcris.thinkingbees.tech'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
 
